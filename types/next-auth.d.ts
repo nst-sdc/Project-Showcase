61b5ab167faf20013b0b859e987fa1aef6d23ec4
Arpit Sarang <arpit.01.sarang.2005@gmail.com>
2025-04-08 13:12:24 +0530

    Update next-auth.d.ts

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,24 +1,24 @@
 import NextAuth from "next-auth";
 
 declare module "next-auth" {
-  /**
-   * Extend the built-in session types
-   */
   interface Session {
     user: {
       id: string;
       name?: string | null;
       email?: string | null;
       image?: string | null;
-      role?: string;
-    }
+    };
   }
 
-  /**
-   * Extend the built-in user types
-   */
   interface User {
     id: string;
-    role?: string;
+    name?: string | null;
+    email?: string | null;
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    id: string;
   }
 }
