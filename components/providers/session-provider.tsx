7307c963e5cae3fc16ea6a66541ee7f7e08a9db6
Birajit Saikia <birajitsaikia@gmail.com>
2025-06-18 22:41:40 +0530

    Clean up

--- components/providers/session-provider.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { SessionProvider as NextAuthSessionProvider } from "next-auth/react";
-import { PropsWithChildren } from "react";
-
-export function SessionProvider({ children }: PropsWithChildren) {
-  return <NextAuthSessionProvider>{children}</NextAuthSessionProvider>;
-}
