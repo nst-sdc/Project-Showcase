e32dabb7d9f79412ed76e94ffaff455909c3803a
Shalini Saloni <shalinisaloni2801@gmail.com>
2025-06-28 21:25:21 +0530

    Added Home Page

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,6 @@ function App() {
 
   return (
     <div className="app-wrapper">
-      <Navbar onNavClick={setActivePage} />
       <main className="main-content">{renderPage()}</main>
       <Footer />
     </div>
